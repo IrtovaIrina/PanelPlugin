ef710c386ee28f77c684bab354a369d798746c2d
irina <irishkapos16@gmail.com>
2023-07-17 11:51:37 +0300

    tree

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.internal.impldep.org.junit.experimental.categories.Categories.CategoryFilter.exclude
+import java.util.regex.Pattern.compile
+
 plugins {
     id("java")
     id("org.jetbrains.intellij") version "1.10.1"
@@ -9,7 +12,12 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
+dependencies{
+    implementation ("io.socket:socket.io-client:2.1.0"){
+    }
+    implementation("org.apache.commons:commons-lang3:3.0")
 
+}
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
@@ -39,5 +47,6 @@ tasks {
 
     publishPlugin {
         token.set(System.getenv("PUBLISH_TOKEN"))
+
     }
 }
--- src/main/java/pro/sky/skyplagin/PanelContent.java
+++ src/main/java/pro/sky/skyplagin/PanelContent.java
@@ -5,35 +5,31 @@ import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.treeStructure.Tree;
+import io.socket.engineio.client.transports.WebSocket;
 import org.jetbrains.annotations.Nullable;
 import com.intellij.openapi.project.Project;
+import io.socket.client.IO;
+import io.socket.client.Socket;
 
-
-import javax.lang.model.element.Element;
 import javax.swing.*;
-import javax.swing.filechooser.FileSystemView;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.tree.*;
-import javax.swing.tree.DefaultTreeModel;
-import javax.swing.tree.TreeNode;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.net.URI;
 import java.util.Arrays;
-import java.util.stream.Collectors;
 
 public class PanelContent extends DialogWrapper {
+    Socket socket;
+    IO.Options options = IO.Options.builder().setTransports(new String[]{WebSocket.NAME}).build();
+    URI url = URI.create("wss://ws.postman-echo.com/socketio");
     private JTextField value1TextField;
     private JTextField value2TextField;
     private JButton getResultButton;
     private JButton getSocketButton;
     private JButton getTreeButton;
-    //private JTextArea resultTextArea;
     private JPanel SkyPanel;
     private JFormattedTextField formattedTextField1;
     private JTextArea textArea1;
-
-    //private JFormattedTextField formattedTextField2;
 
     public PanelContent(@Nullable Project project){
         super(true);
@@ -56,10 +52,21 @@ public class PanelContent extends DialogWrapper {
                 String projectName = project.getName();
                 File file = new File(project.getBasePath());
                 StringBuilder sb = new StringBuilder();
-                sb.append(file.getName() + "\n");
+                sb.append(file.getName()).append("\n");
                 int count = 0;
                 doFilePath(count, sb , file.listFiles());
                 textArea1.setText(sb.toString());
+            }
+        });
+
+        getSocketButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                    try {
+                        connectToSocket();
+                    } catch (InterruptedException ex) {
+                        throw new RuntimeException(ex);
+                    }
             }
         });
     }
@@ -76,8 +83,16 @@ public class PanelContent extends DialogWrapper {
         }
     }
 
+    private void connectToSocket() throws InterruptedException {
+        socket = IO.socket(url, options);
+        socket.on(Socket.EVENT_CONNECT, args -> formattedTextField1.setText("socket connected"));
+        socket.on(Socket.EVENT_DISCONNECT, args -> formattedTextField1.setText("socket disabled"));
+        socket.connect();
+    }
+
     @Override
     protected @Nullable JComponent createCenterPanel() {
         return SkyPanel;
     }
+
 }
